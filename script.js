72c5ab3a73f4e99e0724a4f827ca601334b946f1
Dylan Hagens <dylan.hagens@gmail.com>
2023-03-23 13:23:08 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordInput = document.querySelector("#password-input")
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+="
+  for (var i =0; i < 8; i++) {
+    password += possible.charAt (Math.floor(Math.random() * possible.length))
+  }
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
